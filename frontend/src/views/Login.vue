e63b242445ea2f265ae9d9c3f113a39f249738bf
blablasean <blablablasean@gmail.com>
2026-07-15 10:41:59 +0700

    new login

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login-container">
+    <form @submit.prevent="login" class="login-form">
+      <h2>Login ke AsetKu</h2>
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username" required />
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
